627f32c9dceaaafaf7a8506ce1703a7596e49927
Martins Akeredolu <olamideakeredolu@gmail.com>
2020-10-06 12:01:27 +0100

    created a button to change the theme

--- src/js/components/GameButtons.js
+++ src/js/components/GameButtons.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import NewGameButton from './NewGameButton'
-import PauseGame from './PauseGame';
+import ChangeTheme from './ChangeTheme';
 
 const GameButtons = () => {
     return(
         <div className="game-buttons col-md-2">
             <NewGameButton />
-            <PauseGame />
+            <ChangeTheme />
         </div>
     )
 }
