345f129ce448caa51a8a32d4033f43d132a19ff7
Olamide Akeredolu-Martins <olamideakeredolu@gmail.com>
2021-05-13 00:14:47 +0100

    Made the dark theme the default

--- src/js/contexts/ThemeContext.js
+++ src/js/contexts/ThemeContext.js
@@ -1,28 +1,28 @@
-import React, { useState, createContext, useEffect } from 'react';
+import React, { useState, createContext, useEffect } from "react";
 
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = (props) => {
-    const [isDark, setIsDark] = useState(false)
-    const [darkTheme] = useState({
-        text: ' rgb(211, 208, 208)',
-        cellColor: 'rgb(76, 69, 69)',
-        inactiveCell: ' rgb(211, 208, 208)',
-        boardBackground: 'rgb(17, 15, 15)',
-        buttonColor: ' rgb(76, 69, 69)',
-        activePlayer: 'green',
-        boardBorder: '10px solid rgb(17, 15, 15)'
-    })
-    const changeTheme = () => {
-        let newTheme = !isDark
-        setIsDark(newTheme)
-    }
+  const [isDark, setIsDark] = useState(true);
+  const [darkTheme] = useState({
+    text: " rgb(211, 208, 208)",
+    cellColor: "rgb(76, 69, 69)",
+    inactiveCell: " rgb(211, 208, 208)",
+    boardBackground: "rgb(17, 15, 15)",
+    buttonColor: " rgb(76, 69, 69)",
+    activePlayer: "green",
+    boardBorder: "10px solid rgb(17, 15, 15)",
+  });
+  const changeTheme = () => {
+    let newTheme = !isDark;
+    setIsDark(newTheme);
+  };
 
-    return (
-        <ThemeContext.Provider value={{isDark, changeTheme, darkTheme}}>
-            {props.children}
-        </ThemeContext.Provider>
-    );
-}
- 
+  return (
+    <ThemeContext.Provider value={{ isDark, changeTheme, darkTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
 export default ThemeContextProvider;
